6738a6d87fdf3ec2f51fcd4969147f84554f2f5e
zyx303 <2091183827@qq.com>
2024-06-05 21:08:11 +0800

    为测试键盘模块写的顶层模块

--- kb_top.v
+++ kb_top.v
@@ -3,22 +3,49 @@ module kb_top(
     input rst,                 //重置
     input key_data,
     input key_clk,
-    output [4:0]led
+    // output [4:0]led
+    output led
 );
 
 wire a,s,k,l,enter;
 
-keyboard kb0(
-    .clk(clk),
+// keyboard kb0(
+//     .clk(clk),
+//     .rst(rst),
+//     .key_clk(key_clk),
+//     .key_data(key_data),
+//     .a(led[0]),
+//     .s(led[1]),
+//     .k(led[2]),
+//     .l(led[3]),
+//     .enter(led[4])
+// )
+wire [7:0] key_byte;
+wire state;
+keyboard1 uut (
+    .clk_in(clk),
     .rst(rst),
     .key_clk(key_clk),
     .key_data(key_data),
-    .a(a),
-    .s(s),
-    .k(k),
-    .l(l),
-    .enter(enter)
-)
+    .key_ascii(key_byte),
+    .key_state(led)
+);
+
+// always @(posedge clk) begin
+//     case(key_byte)
+//         8'h1c: a <= 1;
+//         8'hf0: a <= 0;
+//         8'h1b: s <= 1;
+//         8'hf0: s <= 0;
+//         8'h25: k <= 1;
+//         8'hf0: k <= 0;
+//         8'h26: l <= 1;
+//         8'hf0: l <= 0;
+//         8'h5a: enter <= 1;
+//         8'hf0: enter <= 0;
+//         default: ;
+
+// end
 
 
 
